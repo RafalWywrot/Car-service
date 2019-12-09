75d3bd01c1e708cc727f3f26d53d67d0e7dd356c
Rafal <Rafal.Wywrot@onet.pl>
2019-12-09 20:33:18 +0100

    init repository

---
+++ CarService/CarService.WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarService.WebApplication.Startup))]
+namespace CarService.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
