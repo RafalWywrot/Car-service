9caa192590ddeb999fd967b19d84db33751360b1
Rafal <Rafal.Wywrot@onet.pl>
2019-12-15 02:19:29 +0100

    add architecture of migrations

---
+++ CarService/CarService.Repository/Entities/FirstMappedClass.cs
@@ -0,0 +1,9 @@
+﻿namespace CarService.Repository.Entities
+{
+    public class FirstMappedClass
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Surname { get; set; }
+    }
+}
---
+++ CarService/CarService.Repository/Repositories/TestRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarService.Repository.Abstract;
+using CarService.Repository.Entities;
+
+namespace CarService.Repository.Repositories
+{
+    public class TestRepository : ITestRepository
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public TestRepository(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public FirstMappedClass Get()
+        {
+            return unitOfWork.Session.QueryOver<FirstMappedClass>().SingleOrDefault();
+        }
+    }
+}
