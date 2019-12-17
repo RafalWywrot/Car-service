3b1cb75c268ed90a6447e5b3dd078d70349690cd
Rafal <Rafal.Wywrot@onet.pl>
2019-12-17 22:50:11 +0100

    add datables library with polish translation

---
+++ CarService/CarService.WebApplication/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using CarService.Repository.Repositories;
+using CarService.WebApplication.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarService.WebApplication.Areas.Admin.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private ApplicationUserManager _userManager;
+        private readonly ITestRepository testRepository;
+
+        public UsersController(ApplicationUserManager userManager, ITestRepository testRepository)
+        {
+            _userManager = userManager;
+            this.testRepository = testRepository;
+        }
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            var model = testRepository.Get();
+            return View(model);
+        }
+    }
+}
