5da7f75943007f1fba4a245cf9ee61f8b7cc9925
Rafal <Rafal.Wywrot@onet.pl>
2019-12-21 01:08:19 +0100

    add fueltype entity

---
+++ CarService/CarService.Repository/Entities/FuelType.cs
@@ -0,0 +1,8 @@
+﻿namespace CarService.Repository.Entities
+{
+    public class FuelType
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
---
+++ CarService/CarService.Repository/Mappings/FuelTypeMap.cs
@@ -0,0 +1,16 @@
+﻿using CarService.Repository.Entities;
+using FluentNHibernate.Mapping;
+
+namespace CarService.Repository.Mappings
+{
+    public class FuelTypeMap : ClassMap<FuelType>
+    {
+        public FuelTypeMap()
+        {
+            Table("FUEL_TYPE");
+
+            Id(x => x.Id);
+            Map(x => x.Name, "Name");
+        }
+    }
+}
