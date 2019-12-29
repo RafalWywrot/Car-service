0ef7af7a97dea932b8dbc7c3454f51881b7f3fc2
Rafal Wywrot <rafal.wywrot@onet.pl>
2019-12-29 11:26:41 +0100

    Feature/add resources (#17)
    * add resource file
    
    * add methods to change all status

--- CarService/CarService.Logic/Services/Abstract/IBookingService.cs
+++ CarService/CarService.Logic/Services/Abstract/IBookingService.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarService.Logic.Services.Abstract
+﻿namespace CarService.Logic.Services.Abstract
 {
     public interface IBookingService
     {
         /// <param name="id">Booking service id</param>
         void SetStatusAsVerified(int id);
+        void SetStatusAsAccepted(int id);
+        void SetStatusAsDeclined(int id);
+        void SetStatusInProgress(int id);
+        void SetStatusAsFinished(int id);
     }
 }
