2db2c7b18e14d47c754945115533c074b8c43cd9
Rafal <Rafal.Wywrot@onet.pl>
2020-01-14 20:23:47 +0100

    add 'show' page on user services

---
+++ CarService/CarService.WebApplication/Models/ServiceBooking/ServiceBookingDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CarService.Logic.ModelsDTO;
+using CarService.WebApplication.Models.User;
+
+namespace CarService.WebApplication.Models.ServiceBooking
+{
+    public class ServiceBookingDetailViewModel
+    {
+        public ServiceBookingSummaryViewModel ServiceDetails { get; set; }
+        public CarSummaryDTO CarDetails { get; set; }
+        public string MechanicPhoneNumber { get; set; }
+    }
+}
