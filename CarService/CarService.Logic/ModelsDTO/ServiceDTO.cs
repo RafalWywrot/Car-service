e3aabf2bb887b85e013fd185185734d65a46ecef
Rafal <Rafal.Wywrot@onet.pl>
2020-01-18 00:53:17 +0100

    Add new properties to service entity
    Add RequiredComment, MessageUser, Active

---
+++ CarService/CarService.Logic/ModelsDTO/ServiceDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CarService.Logic.ModelsDTO
+{
+    public class ServiceDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool RequiredComment { get; set; }
+        public string MessageUser { get; set; }
+        public bool Active { get; set; } 
+    }
+}
--- CarService/CarService.Repository/Entities/Service.cs
+++ CarService/CarService.Repository/Entities/Service.cs
@@ -6,6 +6,9 @@ namespace CarService.Repository.Entities
     {
         public virtual int Id { get; set; }
         public virtual string Name { get; set; }
+        public virtual bool RequiredComment { get; set; }
+        public virtual string MessageUser { get; set; }
+        public virtual bool Active { get; set; }
 
         public virtual IEnumerable<BookingServiceEntity> BookingServices { get; set; }
     }
